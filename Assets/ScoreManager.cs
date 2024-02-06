d86cf9e7bd8092974920d3ee73f5c241d590c6d8
seandfannin <93547432+seandfannin@users.noreply.github.com>
2024-02-06 13:17:00 -0500

    Added Score keeper and updated back ground
    Updated background and created a score manager script

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public Text scoreText;
+
+    int score = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text ='$'+ score.ToString();
+        
+    }
+
+    // Update is called once per frame
+    public void addPoint()
+    {
+        score += 1;
+        scoreText.text = '$' + score.ToString();
+    }
+}
